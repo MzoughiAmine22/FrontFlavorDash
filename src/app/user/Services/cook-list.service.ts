cf6771edd10a7a4512bf6788e6d0056352f5e284
omar zairi <zairiomar044@gmail.com>
2023-11-24 17:15:54 +0100

    added cooklist and shoppingList services

--- src/app/user/Services/cook-list.service.ts
+++ src/app/user/Services/cook-list.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CookList } from '../Models/cook-list';
 
+const URL = 'http://localhost:5000/api/cooklist/';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CookListService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getAllCooklists(): Observable<CookList[]> {
+    return this.http.get<CookList[]>(URL + 'all');
+  }
+
+  public getUserCooklist(token: string): Observable<CookList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<CookList>(URL, { headers });
+  }
+  public addRecipeToCooklist(id: string, token: string): Observable<CookList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<CookList>(URL + id, null, { headers });
+  }
+  public deleteRecipeFromCooklist(
+    id: string,
+    token: string
+  ): Observable<CookList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete<CookList>(URL + id, { headers });
+  }
 }
--- src/app/user/Services/shopping-list.service.ts
+++ src/app/user/Services/shopping-list.service.ts
@@ -1,9 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ShoppingList } from '../Models/shopping-list';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  private url = 'http://localhost:5000/api/shopping';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createShoppingList(token: string, body: Object): Observable<ShoppingList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<ShoppingList>(`${this.url}/`, body, { headers });
+  }
+
+  getAllShoppingLists(): Observable<ShoppingList[]> {
+    return this.http.get<ShoppingList[]>(`${this.url}/`);
+  }
+
+  getShoppingListById(token: string, id: string): Observable<ShoppingList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<ShoppingList>(`${this.url}/${id}`, { headers });
+  }
+
+  getShoppingListByUserId(
+    token: string,
+    userId: string
+  ): Observable<ShoppingList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<ShoppingList>(`${this.url}/user/${userId}`, {
+      headers,
+    });
+  }
+
+  deleteShoppingList(token: string, id: string): Observable<ShoppingList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete<ShoppingList>(`${this.url}/${id}`, { headers });
+  }
+
+  updateShoppingList(
+    token: string,
+    id: string,
+    body: Object
+  ): Observable<ShoppingList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put<ShoppingList>(`${this.url}/${id}`, body, { headers });
+  }
+
+  deleteIngredientFromShoppingList(
+    token: string,
+    userId: string,
+    ingredientId: string,
+    quantity: number
+  ): Observable<ShoppingList> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete<ShoppingList>(
+      `${this.url}/${userId}/${ingredientId}/${quantity}`,
+      { headers }
+    );
+  }
 }
